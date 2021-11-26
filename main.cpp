9c335247a62d7b0afccbb6dc9b78d55546dc6794
HerikHenrique <74871202+HerikHenrique@users.noreply.github.com>
2021-11-26 19:52:46 -0300

    Criando Arvore

--- main.cpp
+++ main.cpp
@@ -15,41 +15,59 @@ struct paciente
     char email;
 };
 
-std::string infectado()
+string infectado(string resultado1)
     {
-        // bool s = true,n = false;
-        string resultado;
+        std::string resultado;
         printf("Seu teste de Covid-19 deu Positivo?\n");
         cout << "Digite (S) se sim" <<endl << "Digite (N) se não" << endl;
+        // scanf("%s", &resultado);
         cin >> resultado;
 
         std::string key = "s";
         std::string negative = "n";
 
-        // int comparator = resultado.compare(key);
-        // int comparatorNegative = resultado.compare(negative);
+        int comparator = resultado.compare(key);
+        int comparatorNegative = resultado.compare(negative);
 
 
         if (resultado.compare(key) && resultado.compare(negative))
         {
-            cout << "Argumento Inválido\n\n\n\n\n"; 
-            return infectado();
+            cout << "Argumento Inválido\n\n\n\n\n";
+            return infectado(resultado);
         } else if (resultado.compare(key)){
 
             printf("Parabéns! Continue com as devidas precauções");
         }else{
             cout << "Você está infectado!";
-            return resultado;
         }
-        
-        
-    }; 
+            return resultado;
+
+
+    };
+
 
 int main()
 {
-    
+    string resultado1;
+    string positivo = "s";
     setlocale(LC_ALL, "portuguese");
-    infectado();
+
+    string resultadoTeste = infectado(resultado1);
+    // int checkarResultado = strcmp(positivo, resultadoTeste);
+    if (positivo == resultadoTeste)
+    {
+        cout << "Positivo porra!";
+    };
+    
+    cout << resultadoTeste;
+
+    
+    
+
+
+
+
+
 
 
     return 0;
